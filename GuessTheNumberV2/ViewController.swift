faf7a83bf56343947bbdb7bc7b1aca0112ab4187
Victor Dolgov <dolgov.vic95@gmail.com>
2021-11-20 16:14:02 +0700

    Initial Commit

---
+++ GuessTheNumberV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessTheNumberV2
+//
+//  Created by Admin on 20.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
